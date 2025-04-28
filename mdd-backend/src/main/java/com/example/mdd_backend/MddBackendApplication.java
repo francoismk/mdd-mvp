85a57ee8544236df5db9d2679f75c007526a546a
francoismk <francois.szymczak@gmail.com>
2025-04-28 14:27:02 +0200

    initial commit: frontend, backend, docker setup

---
+++ mdd-backend/src/main/java/com/example/mdd_backend/MddBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mdd_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MddBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MddBackendApplication.class, args);
+	}
+
+}
---
+++ mdd-backend/src/test/java/com/example/mdd_backend/MddBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mdd_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MddBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
