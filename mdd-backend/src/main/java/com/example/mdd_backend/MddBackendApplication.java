eb419aff59f16eaabd050ec4fc0e0b5abe943e95
francoismk <francois.szymczak@gmail.com>
2025-05-09 16:17:35 +0200

    feat(config): add some config to launch project

--- mdd-backend/src/main/java/com/example/mdd_backend/MddBackendApplication.java
+++ mdd-backend/src/main/java/com/example/mdd_backend/MddBackendApplication.java
@@ -1,13 +1,19 @@
 package com.example.mdd_backend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MddBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MddBackendApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(
+        MddBackendApplication.class
+    );
 
+    public static void main(String[] args) {
+        log.info("⚠️ Coucou depuis l'application");
+        SpringApplication.run(MddBackendApplication.class, args);
+    }
 }
