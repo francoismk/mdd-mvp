c62640069a23fb07ec0544f50714535736b5ae59
francoismk <francois.szymczak@gmail.com>
2025-05-10 16:32:42 +0200

    feat(service): continue to work on user service

--- mdd-backend/src/main/java/com/example/mdd_backend/services/UserService.java
+++ mdd-backend/src/main/java/com/example/mdd_backend/services/UserService.java
@@ -1,7 +1,9 @@
 package com.example.mdd_backend.services;
 
+import com.example.mdd_backend.dtos.CreateUserDTO;
 import com.example.mdd_backend.models.DBUser;
 import com.example.mdd_backend.repositories.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +12,19 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
 
     public UserService(
         UserRepository userRepository,
-        PasswordEncoder passwordEncoder
+        PasswordEncoder passwordEncoder,
+        ModelMapper modelMapper
     ) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.modelMapper = modelMapper;
     }
 
-    public void createUser(DBUser user) {}
+    public void createUser(CreateUserDTO userDTO) {
+        DBUser user = modelMapper.map(userDTO, DBUser.class);
+    }
 }
