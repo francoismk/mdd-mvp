4cec0cac2c3556ab273af1108964f34f540d606b
francoismk <francois.szymczak@gmail.com>
2025-05-10 19:51:39 +0200

    feat(user): add create, get all and get by id method for first test

--- mdd-backend/src/main/java/com/example/mdd_backend/services/UserService.java
+++ mdd-backend/src/main/java/com/example/mdd_backend/services/UserService.java
@@ -1,8 +1,13 @@
 package com.example.mdd_backend.services;
 
 import com.example.mdd_backend.dtos.CreateUserDTO;
+import com.example.mdd_backend.dtos.GetUserDTO;
 import com.example.mdd_backend.models.DBUser;
 import com.example.mdd_backend.repositories.UserRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -26,5 +31,26 @@ public class UserService {
 
     public void createUser(CreateUserDTO userDTO) {
         DBUser user = modelMapper.map(userDTO, DBUser.class);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    public List<GetUserDTO> getAllUsers() {
+        List<DBUser> users = userRepository.findAll();
+        List<GetUserDTO> userDTOs = new ArrayList<>();
+
+        for (DBUser user : users) {
+            userDTOs.add(modelMapper.map(user, GetUserDTO.class));
+        }
+
+        return userDTOs;
+    }
+
+    public GetUserDTO getUserById(UUID id) {
+        Optional<DBUser> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            return modelMapper.map(user, GetUserDTO.class)
+        }
+        return null;
     }
 }
