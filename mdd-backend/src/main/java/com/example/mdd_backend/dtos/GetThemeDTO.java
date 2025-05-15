f9224dc005e2581b29137f30416300836e1216ab
francoismk <francois.szymczak@gmail.com>
2025-05-15 20:31:38 +0200

    feat: wip on subscriptions management

---
+++ mdd-backend/src/main/java/com/example/mdd_backend/dtos/GetThemeDTO.java
@@ -0,0 +1,12 @@
+package com.example.mdd_backend.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetThemeDTO {
+    
+    private String id;
+    private String name;
+}
--- mdd-backend/src/main/java/com/example/mdd_backend/services/UserService.java
+++ mdd-backend/src/main/java/com/example/mdd_backend/services/UserService.java
@@ -1,11 +1,18 @@
 package com.example.mdd_backend.services;
 
 import com.example.mdd_backend.dtos.CreateUserDTO;
+import com.example.mdd_backend.dtos.GetThemeDTO;
 import com.example.mdd_backend.dtos.GetUserDTO;
+import com.example.mdd_backend.models.DBTheme;
 import com.example.mdd_backend.models.DBUser;
+import com.example.mdd_backend.repositories.ThemeRepository;
 import com.example.mdd_backend.repositories.UserRepository;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,15 +22,18 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final ThemeRepository themeRepository;
     private final PasswordEncoder passwordEncoder;
     private final ModelMapper modelMapper;
 
     public UserService(
         UserRepository userRepository,
+        ThemeRepository themeRepository,
         PasswordEncoder passwordEncoder,
         ModelMapper modelMapper
     ) {
         this.userRepository = userRepository;
+        this.themeRepository = themeRepository;
         this.passwordEncoder = passwordEncoder;
         this.modelMapper = modelMapper;
     }
@@ -41,15 +51,64 @@ public class UserService {
         List<DBUser> users = userRepository.findAll();
         return users
             .stream()
-            .map(user -> modelMapper.map(user, GetUserDTO.class))
+            .map(user -> {
+                GetUserDTO userDTO = modelMapper.map(user, GetUserDTO.class);
+                if (user.getSubscriptions() != null && !user.getSubscriptions().isEmpty()) {
+                    List<GetThemeDTO> themeDTOs = user.getSubscriptions().stream()
+                        .map(themeid -> {
+                            Optional<DBTheme> themeOptional = themeRepository.findById(themeid);
+                            return themeOptional.map(theme -> modelMapper.map(theme, GetThemeDTO.class)).orElse(null);
+                        })
+                        .filter(themeDTO -> themeDTO != null)
+                        .collect(Collectors.toList());
+                    userDTO.setSubscriptions(themeDTOs);
+                } else {
+                    userDTO.setSubscriptions(new ArrayList<>());
+                }
+                return userDTO;
+            })
             .collect(Collectors.toList());
     }
 
     public GetUserDTO getUserById(String id) {
-        Optional<DBUser> user = userRepository.findById(id);
-        if (user.isPresent()) {
-            return modelMapper.map(user, GetUserDTO.class);
+        DBUser user = userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("user not found with ID : " + id));
+        
+        GetUserDTO userDTO = modelMapper.map(user, GetUserDTO.class);
+
+        if (user.getSubscriptions() != null && !user.getSubscriptions().isEmpty()) {
+            List<GetThemeDTO> themeDTOs = user.getSubscriptions().stream()
+                .map(themeId -> {
+                    Optional<DBTheme> themeOptional = themeRepository.findById(themeId);
+                    return themeOptional.map(theme -> modelMapper.map(theme, GetThemeDTO.class)).orElse(null);
+                })
+                .filter(themeDTO -> themeDTO != null)
+                .collect(Collectors.toList());
+
+                userDTO.setSubscriptions(themeDTOs);
         }
-        return null;
+        else {
+            userDTO.setSubscriptions(new ArrayList<>());
+        }
+        return userDTO;
     }
+
+    public GetThemeDTO subscribeUserToTheme(String userId, String themeId) {
+        DBUser user = userRepository.findById(userId).orElseThrow(() -> new NoSuchElementException("user not found with ID : " + userId));
+        DBTheme theme = themeRepository.findById(themeId).orElseThrow(() -> new NoSuchElementException("theme not found with ID : " + themeId));
+        List<String> themeExisting = user.getSubscriptions();
+
+        if(themeExisting == null) {
+            themeExisting = new ArrayList<>();
+            user.setSubscriptions(themeExisting);
+        }
+
+        if (!themeExisting.contains(theme.getId())) {
+            themeExisting.add(theme.getId());
+            userRepository.save(user);
+        }
+        
+        return modelMapper.map(theme, GetThemeDTO.class);
+        
+    }
+
 }
