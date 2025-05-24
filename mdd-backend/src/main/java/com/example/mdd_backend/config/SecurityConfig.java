4b73fcb7dd3fd8cc973e5a12e98e352d431c0ec6
francoismk <francois.szymczak@gmail.com>
2025-05-24 19:29:48 +0200

    feat(jwt-config): update security filter chain to use jwt auth

--- mdd-backend/src/main/java/com/example/mdd_backend/config/SecurityConfig.java
+++ mdd-backend/src/main/java/com/example/mdd_backend/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.example.mdd_backend.config;
 
+import com.example.mdd_backend.services.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -28,9 +33,19 @@ public class SecurityConfig {
                     .permitAll()
                     .anyRequest()
                     .permitAll()
-            );
+            ).oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer ->
+                        jwtConfigurer.jwtAuthenticationConverter(new JwtAuthenticationConverter())
+            ));
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(CustomUserDetailsService customUserDetailsService) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(daoAuthenticationProvider);
     }
 
     @Bean
