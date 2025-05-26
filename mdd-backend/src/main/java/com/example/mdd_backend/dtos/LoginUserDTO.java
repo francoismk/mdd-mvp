ee8df39a009ec2f90da5ed75bf68157ab192a62c
francoismk <francois.szymczak@gmail.com>
2025-05-26 08:49:17 +0200

    feat(jwt-config): add dto to login user

---
+++ mdd-backend/src/main/java/com/example/mdd_backend/dtos/LoginUserDTO.java
@@ -0,0 +1,19 @@
+package com.example.mdd_backend.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Getter
+@Setter
+public class LoginUserDTO {
+
+    @NotBlank(message = "Email or usernale is required")
+    private String usernameOrEmail;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
