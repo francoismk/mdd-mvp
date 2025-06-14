76114d176e411fa2f0006e7eb117568b9a90dc63
francoismk <francois.szymczak@gmail.com>
2025-06-14 19:14:15 +0200

    feat(register): implement register form

--- mdd-frontend/src/app/app.routes.ts
+++ mdd-frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ArticleListComponent } from "./features/articles/components/article-lis
 import { TopicListComponent } from "./features/topics/components/topic-list.component";
 import {ArticleFormComponent} from './features/articles/components/article-form.component';
 import {AuthFormComponent} from './features/auth/components/auth-form.component';
+import { RegisterFormComponent } from "./features/auth/components/register-from.component";
 
 export const routes: Routes = [
   { path: "articles", component: ArticleListComponent },
   { path: "topics", component: TopicListComponent },
   { path: 'create-article', component: ArticleFormComponent },
-  { path: 'login', component: AuthFormComponent }
+  { path: 'login', component: AuthFormComponent },
+  { path: 'register', component: RegisterFormComponent }
 ];
